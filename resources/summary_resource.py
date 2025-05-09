d2412e7694cb3941ea433164ea6c9f0b9c3d1ac9
Ama-Pathirana <119398010+Ama-Pathirana@users.noreply.github.com>
2025-05-09 11:59:19 +0530

    Update summary_resource.py

--- resources/summary_resource.py
+++ resources/summary_resource.py
@@ -3,11 +3,13 @@ from flask_restful import Resource
 from schemas.sales_summary_schema import HourlySalesSummarySchema
 from services import summary_service
 from flask_jwt_extended import jwt_required
+from app import roles_required  # Import the decorator
 
 summary_schema = HourlySalesSummarySchema(many=True)
 
 class HourlySalesSummaryResource(Resource):
     @jwt_required()
+    @roles_required(['sales_manager', 'head_office_manager'])
     def get(self, store_id):
         summaries = summary_service.get_hourly_summaries_by_store(store_id)
         return summary_schema.dump(summaries)
