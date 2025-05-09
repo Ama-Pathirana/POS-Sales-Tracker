a7b7ad1f8b81d08b44de65ffb23652b08d3c78bf
Ama-Pathirana <119398010+Ama-Pathirana@users.noreply.github.com>
2025-05-09 11:58:56 +0530

    Update inventory_resource.py

--- resources/inventory_resource.py
+++ resources/inventory_resource.py
@@ -3,11 +3,13 @@ from flask_restful import Resource
 from schemas.product_schema import ProductSchema
 from models.product import Product
 from flask_jwt_extended import jwt_required
+from app import roles_required  # Import the decorator
 
 product_schema = ProductSchema()
 
 class InventoryStatusResource(Resource):
     @jwt_required()
+    @roles_required(['cashier', 'sales_manager', 'head_office_manager'])
     def get(self, product_id):
         product = Product.query.get_or_404(product_id)
         return {"product_name": product.product_name, "current_stock": product.stock_quantity}
